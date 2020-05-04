ca8f84309d38ea4bcd895a14cf164fca6c226847
tomual <tomual@users.noreply.github.com>
2020-05-04 19:49:11 +0800

    File operations to be synchronous

--- average.js
+++ average.js
@@ -4,26 +4,20 @@ var config = require('config');
 
 var games = {};
 
-fs.readdir(config.get('ENV.SAVE_PATH'), readdir_callback);
 var date = new Date().toISOString().replace(/(.*)T.*/, "$1").replace(/-/gi, '');
+var files = fs.readdirSync(config.get('ENV.SAVE_PATH'));
 
-function readdir_callback(error, files) {
-	if (error) {
-		return console.log(error)
-	}
-	for (var i = files.length - 1; i >= 0; i--) {
-		if (files[i].includes(date)) {
-			fs.readFile(config.get('ENV.SAVE_PATH') + files[i], 'utf8', readfile_callback);
+for (var i = files.length - 1; i >= 0; i--) {
+	if (files[i].includes(date)) {
+		data = fs.readFileSync(config.get('ENV.SAVE_PATH') + files[i], 'utf8');
+		data = JSON.parse(data);
+		for (var j = data.length - 1; j >= 0; j--) {
+			if (!games[data[j].name]) {
+				games[data[j].name] = [];
+			}
+			games[data[j].name].push({viewers: data[j].viewers, streams: data[j].streams})
 		}
 	}
 }
 
-function readfile_callback(error, data) {
-	data = JSON.parse(data);
-	for (var i = data.length - 1; i >= 0; i--) {
-		if (!games[data[i].name]) {
-			games[data[i].name] = [];
-		}
-		games[data[i].name].push({viewers: data[i].viewers, streams: data[i].streams})
-	}
-}
+console.log(games)
